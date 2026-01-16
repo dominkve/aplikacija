47c89413a485663e316c02eeba4f1602c87cda93
dominkve <dominikbogmic8@gmail.com>
2026-01-16 20:43:34 +0100

    add bodovi display and prijave display
    # NAZIVI MISMATCH
    
    - datasets coe from different people so they sometimes don't match

--- src/js/program.js
+++ src/js/program.js
@@ -18,14 +18,22 @@ async function getData(PATH) {
 async function main() {
     const ID = localStorage.getItem("idPrograma");
 
-    let programi = await getData("../public/programi.json");
+    const programi = await getData("../public/programi.json");
+    const bodovi = await getData("../public/bodovi.json");
+    const prijave = await getData("../public/broj_prijava.json");
 
-    let program = programi.filter((obj) => {
+    const program = programi.filter((obj) => {
         return obj.idPrograma == ID;
     })[0];
 
     console.log(program)
     document.querySelector("h1.title").innerHTML = program.naziv;
+
+    document.querySelector("div.card.bodovi > p").innerHTML= bodovi[`${program.naziv}, ${program.mjesto}`];
+    document.querySelector("div.card.prijave > p").innerHTML = prijave.filter((obj) => {
+        return obj.Studij == program.naziv && obj.Mjesto == program.mjesto;
+    })[0]["Broj prijava"];
+    console.log()
 }
 
 main();
